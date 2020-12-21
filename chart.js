2a43d85d6e5ce9a32fd14f1f1648f70b72787dd8
Eva <mt181104@fhstp.ac.at>
2020-12-21 14:21:09 +0100

    breite tests

--- chart.js
+++ chart.js
@@ -32,6 +32,11 @@ async function drawAreaChart(url, xA, yA) {
 
   let test = d3.select('#selectButton').property('value');
 
+  // if (d3.select("#selectButton").property("value") == "1") {
+  //   test = anzahl; 
+  //   console.log(test); 
+  // }
+
   let startDate = dateParser(dataset[dataset.length - test].datum);
   let endDate = dateParser(dataset[dataset.length - 1].datum);
 
@@ -41,34 +46,33 @@ async function drawAreaChart(url, xA, yA) {
   //console.log(startDate); 
   //console.log(endDate);
   //console.log(startValue); 
-  //console.log(dataset.length); 
-  //console.table(dataset);
+  // console.log(dataset.length); 
+  // console.table(dataset);
 
 
   //2 - set dimension and properties
   let dimensions = {
-    width: window.innerWidth,
-    height: window.innerWidth * 0.7,
-    padding: {
+    width: window.innerWidth * 0.9,
+    height: window.innerWidth * 0.5,
+    margin: {
       top: 15,
-      right: 20,
+      right: 40,
       bottom: 60,
       left: 47
     },
   };
   //calculate bounded width and height
-  dimensions.boundW = dimensions.width - dimensions.padding.left - dimensions.padding.right;
-  dimensions.boundH = dimensions.height - dimensions.padding.top - dimensions.padding.bottom;
+  dimensions.boundW = dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundH = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   //3 - draw canvas
   const wrapper = d3.select('#wrapper');
   let svg = wrapper.append('svg')
     .attr('width', dimensions.width)
-    .attr('height', dimensions.height)
-    .attr('overflow', 'hidden');
+    .attr('height', dimensions.height);
 
   let bounds = svg.append('g')
-    .style('transform', `translate(${dimensions.padding.left}px,${dimensions.padding.top}px)`);
+    .style('transform', `translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`);
 
 
   //create scales
@@ -76,6 +80,8 @@ async function drawAreaChart(url, xA, yA) {
     .nice()
     .domain(d3.extent(dataset, yAccessor))
     .range([dimensions.boundH, 0]);
+
+   console.log(startDate); 
 
   let xScale = d3.scaleTime()
     .domain([startDate, endDate])
@@ -88,14 +94,13 @@ async function drawAreaChart(url, xA, yA) {
     .y0(dimensions.boundH)
     .y1(d => yScale(yAccessor(d)));
 
+
   let area = bounds
+    // .enter()
     .append('path')
-    .attr("class", "test")
     .attr('d', areaGenerator(dataset))
     .attr('fill', '#968AB6')
     .attr('stroke', '#968AB6');
-
-  console.log(area); 
 
   //draw peripherals
   let yAxisGenerator = d3.axisLeft()
@@ -103,18 +108,21 @@ async function drawAreaChart(url, xA, yA) {
 
   yAxisGenerator.ticks(4);
   yAxisGenerator.tickSize(-dimensions.boundW); //weißes 'grid'
+  //yAxisGenerator.tickValues([startValue, endValue]);
 
   let yAxis = bounds.append('g')
     .attr("class", "grid")
-    .call(yAxisGenerator); //führt generator methode aus
-    // .style('transform', `translateX(-${dimensions.padding.left}px)`); 
+    .call(yAxisGenerator);//führt generator methode aus
 
   let xAxisGenerator = d3.axisBottom()
     .scale(xScale);
 
-  //xAxisGenerator.ticks(3);
+  xAxisGenerator.ticks(2);
   xAxisGenerator.tickValues([startDate, endDate]);
-  xAxisGenerator.tickFormat(d3.timeFormat("%d.%m"));
+  xAxisGenerator.tickFormat(d3.timeFormat("%d.%m.%Y"));
+
+  // yAxisGenerator.ticks(8);
+  // yAxisGenerator.tickValues([0,endValue+100]);
 
   let xAxis = bounds.append('g')
     .attr("class", "grid")
@@ -154,6 +162,7 @@ async function drawAreaChart(url, xA, yA) {
 
     yAxisGenerator.ticks(4);
     yAxisGenerator.tickSize(-dimensions.boundW); //weißes 'grid'
+    //yAxisGenerator.tickValues([startValue, endValue]);
 
     yAxis = bounds.append('g')
       .attr("class", "grid")
@@ -162,16 +171,15 @@ async function drawAreaChart(url, xA, yA) {
 
     xAxisGenerator.ticks(2);
     xAxisGenerator.tickValues([startDate, endDate]);
-    xAxisGenerator.tickFormat(d3.timeFormat("%d.%m"));
+    xAxisGenerator.tickFormat(d3.timeFormat("%d.%m.%Y"));
 
 
     xAxis.transition().duration(1000).call(xAxisGenerator);
     yAxis.transition().duration(1000).call(yAxisGenerator);
     area.transition().duration(1000).call(areaGenerator);
 
-    d3.select("#wrapper").call(hover);
-
   }
+
 
   d3.select('#selectButton').on('change', updateX);
   /*-------------------------------------------------------------*/
@@ -194,54 +202,54 @@ async function drawAreaChart(url, xA, yA) {
   d3.select("#wrapper").call(hover);
 
   function hover() {
-   var bisect = d3.bisector(d => d.datum).left,
-     format = d3.format("+20"),
-     dateFormat = d3.timeFormat("%d %B")
+    var bisect = d3.bisector(d => d.datum).left,
+      format = d3.format("+20"),
+      dateFormat = d3.timeFormat("%d %B")
 
-   var focus = bounds.append("g")
-     .style("display", "none");
+    var focus = bounds.append("g")
+      .style("display", "none");
 
-   focus.append("line") //linie definieren
-     .attr("stroke", "#666")
-     .attr("stroke-width", 1)
-     .attr("y1", -dimensions.boundH) //obere grenze
-     .attr("y2", -0); //untere grenze
+    focus.append("line") //linie definieren
+      .attr("stroke", "#666")
+      .attr("stroke-width", 1)
+      .attr("y1", -dimensions.boundH) //obere grenze
+      .attr("y2", -0); //untere grenze
 
     focus.append("circle")
-     .attr("class", "circle")
-     .attr("r", 5)
-     .attr("dy", 5)
-     .attr("stroke", "steelblue")
-     .attr("fill", "#fff");
+      .attr("class", "circle")
+      .attr("r", 5)
+      .attr("dy", 5)
+      .attr("stroke", "steelblue")
+      .attr("fill", "#fff");
 
     focus.append("text") //text dfinieren
       .attr("fill", "#666")
       .attr("text-anchor", "middle")
 
-   var overlay = bounds.append("rect") //wo hovern funktioniert
-     .attr("class", "overlay")
-     .attr("x", 0)
-     .attr("y", 0)
-     .attr("width", dimensions.boundW)
-     .attr("height", dimensions.boundH)
-     .on("mouseover", () => focus.style("display", null))
-     .on("mouseout", () => focus.style("display", "none"))
-     .on("mousemove", mousemove);
+    var overlay = bounds.append("rect") //wo hovern funktioniert
+      .attr("class", "overlay")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", dimensions.boundW)
+      .attr("height", dimensions.boundH)
+      .on("mouseover", () => focus.style("display", null))
+      .on("mouseout", () => focus.style("display", "none"))
+      .on("mousemove", mousemove);
 
-   function mousemove() { //was beim hovern angezeigt wird 
+    function mousemove() { //was beim hovern angezeigt wird 
 
       var xM = d3.mouse(this)[0];
       var yM = d3.mouse(this)[1];
 
       var x0 = xScale.invert(xM),
-          i = (dataset, x0, 1),
-          d0 = dataset[i - 1],
-          d1 = dataset[i],
-          d = x0 - d0.datum > d1.datum - x0 ? d1 : d0;
-      focus.attr("transform", `translate(${xM},${yScale(d.taeglicheErkrankungen)})`); 
+        i = (dataset, x0, 1),
+        d0 = dataset[i - 1],
+        d1 = dataset[i],
+        d = x0 - d0.datum > d1.datum - x0 ? d1 : d0;
+      focus.attr("transform", `translate(${xM},${yScale(d.taeglicheErkrankungen)})`);
 
       console.log(yScale(d.taeglicheErkrankungen));
-   }
+    }
   }
   /*-------------------------------------------------------------*/
 
